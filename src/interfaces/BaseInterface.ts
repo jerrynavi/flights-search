2fd48d924737d913e9dcacc3a91edaad8c3d4426
Jerry Ejonavi <jerryasih@gmail.com>
2019-11-26 23:46:48 +0100

    chore: code cleanup

--- src/interfaces/BaseInterface.ts
+++
@@ -1,5 +0,0 @@
-export interface BaseInterface {
-    id?: number;
-    created_at?: Date;
-    updated_at?: Date;
-}
--- src/interfaces/User.ts
+++
@@ -1,6 +0,0 @@
-import { BaseInterface } from './BaseInterface';
-
-export interface User extends BaseInterface {
-    email: string;
-    api_token: string;
-}
